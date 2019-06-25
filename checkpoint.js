6be86568876d963a3c995de4af953d0251007cb2
rania15 <52164326+rania15@users.noreply.github.com>
2019-06-25 14:07:23 +0100

    Add files via upload

---
+++ checkpoint.js
@@ -0,0 +1,33 @@
+const pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+  
+  let getAge = (pet) => {
+    return new Date().getFullYear() - pet.bornOn;
+  }
+  
+  var petsWithAge = [];
+  pets.forEach((pet) => {
+    pet.age = getAge(pet);
+  
+    petsWithAge.push(pet);
+  })
+  console.log(petsWithAge);
+  
+  var dogs = [];
+ pets.forEach ((pet) => {
+    if (pet.type === "dog") {
+      dogs.push(pet);
+    }
+  })
+  console.log(dogs);
+  
+  var jasper;
+pets.forEach((pet) => {
+    if (pet.name === "Jasper") {
+      jasper = pet;
+    }
+  })
+  console.log(`Jasper is ${jasper.age}   years old`);
